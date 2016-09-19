9277050fe52f110fdead733bc988c72600e30c9d
shiyabing <shiyabing@xiangshang360.com>
2016-09-19 11:19:18 +0800

    个人中心

---
+++ WXWeibo/WXWeibo/Profile/Controller/ProfileViewController.h
@@ -0,0 +1,13 @@
+//
+//  ProfileViewController.h
+//  WXWeibo
+//
+//  Created by liuwei on 15/10/9.
+//  Copyright (c) 2015年 wxhl. All rights reserved.
+//
+
+#import "BaseViewController.h"
+
+@interface ProfileViewController : BaseViewController
+
+@end
